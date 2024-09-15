0b29f945c1ece0c188115b509197f9fb9983691c
Hezron <hezroncr@gmail.com>
2024-09-15 15:37:09 -0600

    Historial y mas

--- src/Components/componentsAdminMenu/DetalleActivo.tsx
+++ src/Components/componentsAdminMenu/DetalleActivo.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaArrowLeft, FaTrash } from 'react-icons/fa';
+import HistorialPrestamos from './HistorialPrestamos';
 
 // Definimos la interfaz para el activo
 interface Asset {
@@ -17,83 +18,108 @@ interface Asset {
 }
 
 const DetalleComponent: React.FC<{ asset: Asset; onBack: () => void }> = ({ asset, onBack }) => {
+  const [activeTab, setActiveTab] = useState('detalle'); // Controla la pestaña activa
+
   const handleEliminar = (id: string) => {
     console.log('Eliminar activo', id);
     // Lógica para eliminar el activo
   };
 
   return (
-    <div className="w-full bg-white shadow-lg rounded-lg p-8 space-y-8 overflow-auto" style={{ maxHeight: '80vh' }}>
-      <div className="flex justify-between items-start w-full">
-        <div className="flex items-start space-x-8 w-full">
-          {/* Imagen del activo */}
-          <div className="flex-shrink-0">
-            <img src={asset.foto} alt="Foto del Activo" className="w-40 h-40 object-cover rounded-lg" />
-          </div>
-          {/* Datos del activo en una fila */}
-          <div className="grid grid-cols-2 gap-6 text-sm leading-6 w-full">
-            <div>
-              <p><strong>No. Identificador:</strong> {asset.id}</p>
-              <p><strong>Descripción:</strong> {asset.descripcion}</p>
-              <p><strong>Marca:</strong> {asset.marca}</p>
-              <p><strong>Modelo:</strong> {asset.modelo}</p>
-            </div>
-            <div>
-              <p><strong>Serie:</strong> {asset.serie}</p>
-              <p><strong>Estado:</strong> {asset.estado}</p>
-              <p><strong>Ubicación:</strong> {asset.ubicacion}</p>
-              <p><strong>Modo de Adquisición:</strong> {asset.modoAdquisicion}</p>
-              <p><strong>Precio:</strong> {asset.precio}</p>
-              <p><strong>Observación:</strong> {asset.observacion}</p>
-            </div>
-          </div>
-        </div>
-
-        {/* Botón de eliminar */}
+    <div className="w-full h-full bg-white shadow-lg rounded-lg p-8 overflow-auto">
+      {/* Pestañas en la parte superior */}
+      <div className="flex justify-center mb-6">
         <button
-          onClick={() => handleEliminar(asset.id)}
-          className="bg-red-600 text-white py-2 px-4 rounded-lg shadow hover:bg-red-700 transition flex items-center"
+          className={`px-4 py-2 mr-2 font-bold ${activeTab === 'detalle' ? 'border-b-2 border-blue-600' : ''}`}
+          onClick={() => setActiveTab('detalle')}
         >
-          <FaTrash className="mr-2" /> Eliminar Activo
+          Detalle de Activo
+        </button>
+        <button
+          className={`px-4 py-2 ml-2 font-bold ${activeTab === 'historial' ? 'border-b-2 border-blue-600' : ''}`}
+          onClick={() => setActiveTab('historial')}
+        >
+          Historial de Préstamos
         </button>
       </div>
 
-      {/* Botón para volver a la tabla */}
-      <button
-        onClick={onBack}
-        className="bg-blue-600 text-white py-2 px-4 rounded-lg shadow hover:bg-blue-700 transition flex items-center"
-      >
-        <FaArrowLeft className="mr-2" /> Volver a la tabla principal
-      </button>
+      {/* Contenido basado en la pestaña seleccionada */}
+      {activeTab === 'detalle' ? (
+        <>
+          {/* Imagen y Datos del Activo */}
+          <div className="flex justify-start items-start w-full space-x-8 mb-4">
+            {/* Imagen del activo */}
+            <div className="flex-shrink-0">
+              <img src={asset.foto} alt="Foto del Activo" className="w-40 h-40 object-cover rounded-lg" />
+            </div>
 
-      {/* Historial de Préstamos */}
-      <div className="mt-4 w-full">
-        <h2 className="text-xl font-bold">Historial Préstamos</h2>
-        <div className="mt-4 space-y-4 overflow-y-auto max-h-40">
-          {/* Ejemplo de cada préstamo */}
-          <div className="bg-gray-100 p-4 rounded-lg">
-            <p><strong>Ana</strong></p>
-            <p>Prestador: Ana Laboratorio#1</p>
-            <p>Prestada: Pablo Laboratorio#2</p>
-            <p>Tiempo: 3 Días</p>
-            <p><strong>Estado: En Préstamo</strong></p>
+            {/* Datos del activo */}
+            <div className="w-full max-h-[400px] overflow-y-auto">
+              <div className="grid grid-cols-8 gap-4 text-xs leading-6">
+                <div className="col-span-1">
+                  <p className="font-bold">No. Identificador</p>
+                  <p>{asset.id}</p>
+                </div>
+                <div className="col-span-2">
+                  <p className="font-bold">Descripción</p>
+                  <p>{asset.descripcion}</p>
+                </div>
+                <div className="col-span-1">
+                  <p className="font-bold">Marca</p>
+                  <p>{asset.marca}</p>
+                </div>
+                <div className="col-span-1">
+                  <p className="font-bold">Modelo</p>
+                  <p>{asset.modelo}</p>
+                </div>
+                <div className="col-span-1">
+                  <p className="font-bold">Serie</p>
+                  <p>{asset.serie}</p>
+                </div>
+                <div className="col-span-1">
+                  <p className="font-bold">Estado</p>
+                  <p>{asset.estado}</p>
+                </div>
+                <div className="col-span-1">
+                  <p className="font-bold">Ubicación</p>
+                  <p>{asset.ubicacion}</p>
+                </div>
+                <div className="col-span-2">
+                  <p className="font-bold">Modo de Adquisición</p>
+                  <p>{asset.modoAdquisicion}</p>
+                </div>
+                <div className="col-span-1">
+                  <p className="font-bold">Precio</p>
+                  <p>{asset.precio}</p>
+                </div>
+                <div className="col-span-2">
+                  <p className="font-bold">Observación</p>
+                  <p>{asset.observacion}</p>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="bg-gray-100 p-4 rounded-lg">
-            <p><strong>Pablo</strong></p>
-            <p>Prestador: Ana Laboratorio#1</p>
-            <p>Prestada: Pablo Laboratorio#2</p>
-            <p>Tiempo: 3 Días</p>
-            <p><strong>Estado: No Devuelto</strong></p>
+
+          {/* Botones de Eliminar y Volver */}
+          <div className="flex justify-end space-x-4 mt-8">
+            <button
+              onClick={() => handleEliminar(asset.id)}
+              className="bg-red-600 text-white py-2 px-4 rounded-lg shadow hover:bg-red-700 transition flex items-center"
+            >
+              <FaTrash className="mr-2" /> Eliminar Activo
+            </button>
+            <button
+              onClick={onBack}
+              className="bg-blue-600 text-white py-2 px-4 rounded-lg shadow hover:bg-blue-700 transition flex items-center"
+            >
+              <FaArrowLeft className="mr-2" /> Volver a la tabla principal
+            </button>
           </div>
-          <div className="bg-gray-100 p-4 rounded-lg">
-            <p><strong>Marco</strong></p>
-            <p>Prestador: Ana Laboratorio#1</p>
-            <p>Prestada: Pablo Laboratorio#2</p>
-            <p>Tiempo: 3 Días</p>
-            <p><strong>Estado: Devuelto</strong></p>
-          </div>
-        </div>
-      </div>
+        </>
+      ) : (
+        /* Historial de Préstamos */
+        <HistorialPrestamos />
+      )}
     </div>
   );
 };
