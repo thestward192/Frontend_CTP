bb94a27aef2af1c72f0556fc9852ed7f22498277
Stward <stwardserrano@gmail.com>
2024-11-11 08:56:51 -0600

    Simplificar configuración de Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  esbuild: {
-    loader: 'tsx',
-    jsx: 'automatic'
-  }
+  plugins: [react()]
 });
