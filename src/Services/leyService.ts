ffb5ab94db4457312e3442090ca1b1e53ff71284
JoseTorres <jtorresjimenez2002@gmail.com>
2024-11-12 12:02:18 -0600

    act. rutas

--- src/Services/leyService.ts
+++ src/Services/leyService.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import { Ley } from '../types/ley';
 
-const API_URL = 'https://backendcontrolactivos-1.onrender.com'; // Ajusta la URL según tu backend
+const API_URL = 'https://backendcontrolactivos-1.onrender.com/'; // Ajusta la URL según tu backend
 
 // Obtener todas las leyes
 export const getLeyes = async (): Promise<Ley[]> => {
