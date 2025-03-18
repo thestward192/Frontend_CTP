cd8b6f97d65633f87f724b1ebd78646e4deff500
Stward <stwardserrano@gmail.com>
2025-03-18 02:02:11 -0600

    Cambios

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -1,7 +1,7 @@
 // src/Services/api.ts
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://backendcontrolactivos-2.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
