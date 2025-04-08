b52b1ef256843be60699ddd92b615d2ac090b767
Stward <stwardserrano@gmail.com>
2025-04-08 00:56:56 -0600

    Merge

--- src/Components/componentsPages/Login.tsx
+++ src/Components/componentsPages/Login.tsx
@@ -29,7 +29,7 @@ const Login: React.FC = () => {
       //const API_URL = 'https://backendcontrolactivos-2.onrender.com/auth/login';
       //const API_URL = 'http://localhost:3000/auth/login';
 
-      const response = await fetch('http://localhost:3000/auth/login', {
+      const response = await fetch('https://backendcontrolactivos-2.onrender.com/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
